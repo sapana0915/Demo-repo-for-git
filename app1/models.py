fd4e31f6d25813d302264a2c8bd6cab17945b48d
Sapana Kautikwar <sapana.kautikwar@atomicloops.com>
2025-10-16 11:39:08 +0530

    refactor : added models.py

--- app1/models.py
+++ app1/models.py
@@ -3,3 +3,19 @@ from django.db import models
 # Create your models here.
 
 
+class Employee(models.Model):
+    Emp_id = models.AutoField(primary_key=True)
+    Emp_name = models.CharField(max_length=100)
+    Emp_email = models.EmailField(max_length=100)
+    Emp_address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Emp_name
+
+class Department(models.Model):
+    Dept_id = models.AutoField(primary_key=True)
+    Dept_name = models.CharField(max_length=100)
+    Dept_location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Dept_name
